808362774148f2f49dc0bd2eac4269fd2073d8fb
iChristwin <ichristwin@gmail.com>
2025-05-09 17:13:05 +0100

    added swagger & redoc api documentation

--- carboncopy/settings/production.py
+++ carboncopy/settings/production.py
@@ -13,7 +13,7 @@ DEBUG = False
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/carboncopy',
         conn_max_age=600
     )
 }
