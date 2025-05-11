dabb023f8395a93916efb51365b658dc85dce3d7
iChristwin <ichristwin@gmail.com>
2025-05-11 14:05:32 +0100

    fixed production deployment settings

--- carboncopy/settings/production.py
+++ carboncopy/settings/production.py
@@ -3,7 +3,6 @@
 # Parse database configuration from $DATABASE_URL
 
 import dj_database_url
-import django_heroku
 
 from .base import *
 
@@ -37,11 +36,9 @@ MIDDLEWARE = [
 # Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
 STATIC_URL = '/static/'
 
-# This production code might break development mode, so we check whether we're in DEBUG mode
-if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+# and renames the files with unique names for each version to support long-term caching
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- carboncopy/wsgi.py
+++ carboncopy/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carboncopy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carboncopy.settings.production')
 
 application = get_wsgi_application()
