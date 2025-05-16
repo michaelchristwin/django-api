e042a05b3ec8e0a82ce8c45dce5d0e01c746b616
iChristwin <ichristwin@gmail.com>
2025-05-16 17:53:00 +0100

    make db migrations

--- metrics/models.py
+++ metrics/models.py
@@ -18,7 +18,7 @@ class Category(models.Model):
     """Model representing categories for metrics."""
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True, null=True)
-    unit = models.CharField(max_length=10)
+    unit = models.CharField(max_length=10, blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -50,7 +50,7 @@ class ProjectMetricMeta(models.Model):
 
 class ProjectMetric(models.Model):
     """Model representing metrics associated with projects."""
-    meta = models.ForeignKey(ProjectMetricMeta, on_delete=models.CASCADE, related_name='metrics')
+    meta = models.OneToOneField(ProjectMetricMeta, on_delete=models.CASCADE, primary_key=True)
     value = models.FloatField()
     timestamp = models.DateTimeField(default=timezone.now)
 
