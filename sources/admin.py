b6083760674a48fb4d6f331ee2f02441193d0f9f
iChristwin <ichristwin@gmail.com>
2025-06-07 11:13:28 +0100

    fixed model action

--- sources/admin.py
+++ sources/admin.py
@@ -7,13 +7,8 @@ from .utils import get_baserow_data
 from .models import Source
 
 
-@admin.register(Source)
-class SourceAdmin(admin.ModelAdmin):
-    list_display = ['name']
-
-
 @admin.action(description='Refresh impact metrics from source')
-def refresh_impact_metrics(modeladmin, request, queryset):
+def refresh(modeladmin, request, queryset):
     impact_data = get_baserow_data('171320', "filter__field_2405062__not_empty&include=Impact Metrics JSON")
     for json_file in impact_data['results']:
         json_url = json_file['Impact Metrics JSON'][0]['url']
@@ -23,3 +18,9 @@ def refresh_impact_metrics(modeladmin, request, queryset):
         for impact in data['impact_data']:
             source = queryset.get(name=impact['source'])
             source.refresh()
+
+
+@admin.register(Source)
+class SourceAdmin(admin.ModelAdmin):
+    actions = [refresh]
+
