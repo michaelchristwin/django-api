5d98f9d621bf846f784bde8ffbd846ef084a50d7
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-06-30 22:10:29 +0100

    Added cors 4

--- carboncopy/settings/base.py
+++ carboncopy/settings/base.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -70,7 +71,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -78,6 +78,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://carboncopy-impact-dashboard.vercel.app",
 ]
 
+CORS_ALLOW_CREDENTIALS = True  # if you're sending cookies or auth headers
 
 ROOT_URLCONF = "carboncopy.urls"
 
