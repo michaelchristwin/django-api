8b1a9b0b8668a9cbc05961bfa351a75df138a9b5
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-07-01 12:25:26 +0100

    Sync changes from source 3

--- metrics/models.py
+++ metrics/models.py
@@ -51,23 +51,23 @@ class ProjectMetric(models.Model):
         return f"{self.meta.project.name} - {self.meta.name} - Metric: {self.value}"
 
     def save(self, *args, **kwargs):
-        if not self.value:
-            return super().save(*args, **kwargs)
-        try:
-            prev = ProjectMetric.objects.values_list("value", flat=True).get(
-                pk=self.pk
-            )  # pull the existing value straight from the DB
-            # wrap in a transaction so that both the save and the delta‐log happen atomically
-            with transaction.atomic():
-                ProjectMetricDelta.objects.create(
-                    meta=self.meta, value=self.value - prev
-                )
-                return super().save(
-                    update_fields=["value", "timestamp"], *args, **kwargs
-                )
-        except ProjectMetric.DoesNotExist:
-            # If the metric does not exist, create a new one
-            return super().save(update_fields=["value", "timestamp"], *args, **kwargs)
+        # Check if this instance already exists in the DB
+        if not self._state.adding:
+            old = ProjectMetric.objects.only("value").get(pk=self.pk)
+            if old.value != self.value:
+                with transaction.atomic():
+                    ProjectMetricDelta.objects.create(
+                        meta=self.meta,
+                        value=self.value - old.value,
+                        timestamp=self.timestamp,
+                    )
+
+                    self.timestamp = timezone.now()
+                    return super().save(
+                        update_fields=["value", "timestamp"], *args, **kwargs
+                    )
+
+        return super().save(*args, **kwargs)
 
 
 class ProjectMetricDelta(models.Model):
